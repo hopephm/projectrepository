c9f8df7ebab14430b74a578b95a5b8c99d7bc3f7
DESKTOP-INKRSO5\hopep <hopephm@naver.com>
2021-12-07 19:53:31 +0900

    프로젝트 구조 변경

--- project-repository/src/main/java/com/hope/projectrepository/repository/CodeLinkRepository.java
+++
@@ -1,9 +0,0 @@
-package com.hope.projectrepository.repository;
-
-import com.hope.projectrepository.domain.CodeLink;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface CodeLinkRepository extends JpaRepository<CodeLink, Long> {
-}
